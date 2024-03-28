6d30bbe1d95af70229e9068cb429b2625baf142f
wowzik1233 <wowzik1233@gmail.com>
2024-03-28 03:16:12 +0300

    Initial Commit

---
+++ Food/FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp.swift
+//  Food
+//
+//  Created by matvey on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
