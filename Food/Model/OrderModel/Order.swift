f720fee0bc587c72fcdece964927a03dec06c3fe
wowzik1233 <wowzik1233@gmail.com>
2024-03-29 04:00:12 +0300

    add order screen and refactoring

---
+++ Food/Model/OrderModel/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  Food
+//
+//  Created by matvey on 29.03.2024.
+//
+
+import Foundation
---
+++ Food/View/Empty List/EmptyList.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyList.swift
+//  Food
+//
+//  Created by matvey on 29.03.2024.
+//
+
+import SwiftUI
+
+struct EmptyList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyList()
+}
