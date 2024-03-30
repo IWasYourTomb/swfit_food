a0edad12368d90160e657ae03901d8f6d8ff975a
wowzik1233 <wowzik1233@gmail.com>
2024-03-30 22:25:07 +0300

    add focus on textFiled

--- Food/Screens/AccountView/AccountView.swift
+++ Food/Screens/AccountView/AccountView.swift
@@ -9,14 +9,25 @@ import SwiftUI
 
 struct AccountView: View {
     @StateObject var vm = AccountViewModel()
+    @FocusState private var focusTextField : FormTextFiled?
+    
     var body: some View {
         NavigationView{
             Form{
                 Section(header: Text("Personal Info")){
                     TextField("First Name", text: $vm.user.firstName)
+                        .focused($focusTextField, equals: .firstName)
+                        .onSubmit {focusTextField = .firstName}
+                        .submitLabel(.next)
                     TextField("Last Name", text: $vm.user.lastName)
+                        .focused($focusTextField, equals: .lastName)
+                        .onSubmit {focusTextField = .lastName}
+                        .submitLabel(.next)
                     TextField("Email", text: $vm.user.email)
+                        .focused($focusTextField, equals: .email)
+                        .onSubmit {focusTextField = nil}
                         .keyboardType(.emailAddress)
+                        .submitLabel(.continue)
                         .autocapitalization(.none)
                         .disableAutocorrection(true)
                     DatePicker("Birthdate", selection: $vm.user.birthdate, displayedComponents: .date)
---
+++ Food/Screens/AccountView/FormTextFiled.swift
@@ -0,0 +1,13 @@
+//
+//  FormTextFiled.swift
+//  Food
+//
+//  Created by matvey on 30.03.2024.
+//
+
+import Foundation
+
+
+enum FormTextFiled{
+    case firstName, lastName, email
+}
